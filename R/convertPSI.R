a7e4a5accd8eec4d30bea54f1d7c4ceb8fa18cbb
Falk Benke <falk.benke@pik-potsdam.de>
2024-09-06 20:17:03 +0200

    increment to dev version

--- R/convertPSI.R
+++ R/convertPSI.R
@@ -21,18 +21,18 @@ convertPSI <- function(x, subtype) {
 
   #PSI data is mapped on iso countries - Note that we do not have regionally differentiated data
   PSI2isoMapFile <- system.file("extdata", "regionmappingISOto21to12.csv",
-    package = "mrtransport", mustWork = TRUE)
+                                package = "mrtransport", mustWork = TRUE)
   PSI2iso <- fread(PSI2isoMapFile, skip = 0)
   setnames(PSI2iso, c("countryCode"), c("iso"))
   PSI2iso <- PSI2iso[, c("iso")][, region := "GLO"]
-  #Here data is mapped on all iso countries -> same vaue according to vehicle class/technology/year
+  #Here data is mapped on all iso countries -> same value according to vehicle class/technology/year
   x <- toolAggregate(x, rel = PSI2iso)
 
   switch(
     subtype,
     "CAPEX" = {
 
-      # PSI CAPEX need to be transformed to USD2017
+      # PSI CAPEX need to be transformed from EUR 2017 to USD 2017
       x <- GDPuc::convertGDP(
         gdp = x,
         unit_in = "constant 2017 €",
